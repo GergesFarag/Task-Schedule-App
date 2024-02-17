5cf061cb834e51d46d49e463f0e89dc09ca0c5e6
Gerges <georgefarag128@gmail.com>
2024-02-18 00:58:13 +0200

    Adding new Functionality : diffrent quotes when achive the goals

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ let addBtn = document.querySelector("#addbtn") ;
 let container = document.querySelector(".task-container");
 let taskList = document.querySelector("#task-list"); 
 let arrOfTasks = [];
+let percentDiv = document.querySelector(".progress");
+let doneCounter;
+let percentQuote = document.querySelector(".progressMSG");
 if(window.localStorage.getItem("tasks")){
     arrOfTasks = JSON.parse(localStorage.getItem("tasks"));
 }
@@ -13,24 +16,58 @@ addBtn.onclick = function(){
         inputField.value = "";
     }
 }
+if(window.localStorage.getItem("counter")){
+    doneCounter = window.localStorage.getItem("counter");
+    if(doneCounter == 0){
+        percentDiv.textContent = `0%`;    
+        DrawQuote(0 ,percentQuote);
+    }else{
+        percentDiv.textContent = `${(doneCounter / arrOfTasks.length * 100).toFixed()}%`;
+        DrawQuote((doneCounter / arrOfTasks.length * 100).toFixed(),percentQuote);
+    }
+}
+else{
+     doneCounter = 0;
+}
 //click on delete button
 taskList.addEventListener("click" , (e)=>{
     if(e.target.classList.contains("delete")){
         e.target.parentElement.parentElement.remove();
         removeFromList(e.target.parentElement.parentElement.getAttribute("data-id"));
+        if(e.target.parentElement.parentElement.classList.contains("done-task")){
+            if(arrOfTasks.length != 0)
+            doneCounter--;
+        }
+        window.localStorage.setItem("counter" , doneCounter);
+        if(doneCounter == 0){
+            percentDiv.textContent = `0%`;   
+            DrawQuote(0,percentQuote); 
+        }else{
+            percentDiv.textContent = `${(doneCounter / arrOfTasks.length * 100).toFixed()}%`
+            DrawQuote((doneCounter / arrOfTasks.length * 100).toFixed(),percentQuote);
+        }
     }
     if(e.target.classList.contains("done")){
         e.target.parentElement.parentElement.classList.toggle("done-task");
         toggleStatusOfTask(e.target.parentElement.parentElement.getAttribute("data-id"));
     }
 })
+
 function toggleStatusOfTask(id){
     for(let i=0; i<arrOfTasks.length; i++){
         if(arrOfTasks[i].id == id){
             if(arrOfTasks[i].completed == false){
                 arrOfTasks[i].completed = true;
+                doneCounter++;
+                window.localStorage.setItem("counter" , doneCounter);
+                percentDiv.textContent = `${(doneCounter / arrOfTasks.length * 100).toFixed()}%`
+                DrawQuote((doneCounter / arrOfTasks.length * 100).toFixed(),percentQuote);
             }else{
                 arrOfTasks[i].completed = false;
+                doneCounter--;
+                window.localStorage.setItem("counter" , doneCounter);
+                percentDiv.textContent = `${(doneCounter / arrOfTasks.length * 100).toFixed()}%`
+                DrawQuote((doneCounter / arrOfTasks.length * 100).toFixed(),percentQuote);
             }
             break;
         }
@@ -39,6 +76,10 @@ function toggleStatusOfTask(id){
 }
 function removeFromList(id){
     arrOfTasks = arrOfTasks.filter((task) => task.id != id);
+    if(arrOfTasks.length == 0){
+        doneCounter = 0;
+        DrawQuote(0,percentQuote); 
+    }
     addToLS(arrOfTasks);
 }
 function addTasksToList(taskText){
@@ -87,4 +128,23 @@ function getFromLS(){
     let tasks = JSON.parse(data);
     addElementsToPageFromArr(tasks);
   }
+}
+function DrawQuote(percentage , div) {
+    let divContent;
+    if (percentage == 0) {
+        divContent = "Can't Wait For Achievements !";
+    } else if (percentage > 0 && percentage <= 30) {
+        divContent = "Awesome, Keep Going Ahead!";
+    } else if (percentage > 30 && percentage <= 50) {
+        divContent = "You're About The Half Of The Day, Continue !";
+    } else if (percentage > 50 && percentage <= 80) {
+        divContent = "Great Job, You Finished More Than The Half !";
+    } else if (percentage > 80 && percentage <= 99) {
+        divContent = "You're About To Finish The Day, Focus !";
+    } else if (percentage == 100) {
+        divContent = "Congratulations, You Have Finished The Goal Of The Day :)";
+    } else {
+        divContent = "Invalid percentage!";
+    }
+    div.textContent = divContent;
 }
